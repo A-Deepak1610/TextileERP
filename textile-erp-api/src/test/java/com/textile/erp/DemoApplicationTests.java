11db5c0269722fce4d33b2a0733077e9b57a5f2c
A-Deepak1610 <deepaka.ad@bitsathy.ac.in>
2026-09-05 23:47:47 +0530

    test: verify spring boot postgresql database connectivity

--- textile-erp-api/src/test/java/com/textile/erp/DemoApplicationTests.java
+++ textile-erp-api/src/test/java/com/textile/erp/DemoApplicationTests.java
@@ -1,13 +1,31 @@
 package com.textile.erp;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private DataSource dataSource;
+
 	@Test
 	void contextLoads() {
+		assertThat(dataSource).isNotNull();
+	}
+
+	@Test
+	void databaseConnectionIsHealthy() throws SQLException {
+		try (Connection connection = dataSource.getConnection()) {
+			assertThat(connection.isValid(2)).isTrue();
+			assertThat(connection.getCatalog()).isEqualTo("texforge");
+		}
 	}
 
 }
